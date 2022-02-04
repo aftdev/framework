4ab1730d800e3d9c8bdbf943d9a71691e96ba572
Christophe G <1050197+cgaube@users.noreply.github.com>
2022-02-03 21:35:19 -0800

    ci: improve coverage commands

--- env/aws/init.sh
+++ env/aws/init.sh
@@ -3,6 +3,6 @@ set -x
 awslocal s3 mb s3://default
 
 # Create dummy file
-echo "testFile s3" >> test.txt
+echo "testFile s3" > test.txt
 # Create a file if you don't already have it
 awslocal s3 cp test.txt s3://default
